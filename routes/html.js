df157075355c62e12295da8ff4a03860a8c7e2db
davidpom <davidpom@gmail.com>
2019-09-16 12:04:27 -0400

    cleanup

--- routes/html.js
+++ routes/html.js
@@ -4,11 +4,9 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "views/index.html"));
     });
-
     app.get("/saved/all", function (req, res) {
         res.sendFile(path.join(__dirname, "views/saved.html"));
     });
-
     app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "views/index.html"));
     });
