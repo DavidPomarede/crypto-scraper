3c0b6c49574c65531ca64b2a2001f2a01974e5fd
davidpom <davidpom@gmail.com>
2019-09-16 12:38:09 -0400

    mongoose2

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-var axios = require("axios");
 var mongoose = require("mongoose");
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
-var Save = require("./models/Save.js");
 var logger = require("morgan");
-var cheerio = require("cheerio");
 var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
-
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true },  { useUnifiedTopology: true } );
-
 
 var exphbs = require("express-handlebars");
 
