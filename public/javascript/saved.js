3eaf451dc2313babd64cf9c2ee8fd1b24a4649f0
davidpom <davidpom@gmail.com>
2019-09-15 20:06:28 -0400

    modal stuff

--- public/javascript/saved.js
+++ public/javascript/saved.js
@@ -116,10 +116,39 @@ $(document).ready(function () {
     });
 
 
+//     // When you click the savenote button
+// $(document).on("click", ".create-note", function() {
+//     // Grab the id associated with the article from the submit button
+//     var thisId = $(this).attr("data-id");
+  
+//     // Run a POST request to change the note, using what's entered in the inputs
+//     $.ajax({
+//       method: "GET",
+//       url: "/notes/" + thisId,
+//       data: {
+//         // Value taken from title input
+//         title: $("#titleinput").val(),
+//         // Value taken from note textarea
+//         body: $("#bodyinput").val()
+//       }
+//     })
+//       // With that done
+//       .then(function(data) {
+//         // Log the response
+//         console.log(data);
+//         // Empty the notes section
+//         $("#notes").empty();
+//       });
+  
+//     // Also, remove the values entered in the input and textarea for note entry
+//     $("#titleinput").val("");
+//     $("#bodyinput").val("");
+//   });
+
     $(document).on("click", ".create-note", function (data) {
         $("#savenote").attr("data-id", $(this).attr("data-id"));
         var thisId = $(this).attr("data-id");
-        var noteTitle = "Notes for the Article: " + thisId;
+        var noteTitle = "Notes:";
         $("#display-title").empty();
         $("#display-title").text(noteTitle);
         $("#textarea1").val("");
